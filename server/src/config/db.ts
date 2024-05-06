0ccbfad80c82c8945bfd38ddce42f58593ac2e53
CipiVlad <supercoder.cipi@gmail.com>
2024-05-06 14:17:47 +0200

    test create pool for fixing server timeout

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,23 +2,33 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
+//     host: process.env.host,
+//     user: process.env.user,
+//     password: process.env.password,
+//     database: process.env.database
+
+// });
+
+// try {
+//     db.connect((err: any) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(`Connected to ${process.env.host}`);
+//         }
+//     })
+// } catch (error) {
+
+// }
+// export default db
+
+//create pool of connections
+const db = mysql.createPool({
     host: process.env.host,
     user: process.env.user,
     password: process.env.password,
     database: process.env.database
-
 });
 
-try {
-    db.connect((err: any) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(`Connected to ${process.env.host}`);
-        }
-    })
-} catch (error) {
-
-}
 export default db
